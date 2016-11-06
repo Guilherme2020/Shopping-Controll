074e76b60e6d3ef9d7910385e96d387787ff213b
guilherme2020 <guilhermerodrigues123477@gmail.com>
2016-11-06 03:49:11 -0200

    primeira versao

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shopping-Controll
+
+Aplicação de controle de compras utilizando na prática os recursos do Javascript. 
---
+++ course/js/config.js
@@ -0,0 +1,14 @@
+
+
+function setConfig() {
+	var texts = {
+		"title":"App control"
+	}
+
+	document.title = texts.title;
+	document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+
+
+setConfig();
